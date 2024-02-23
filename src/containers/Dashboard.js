422dd00b816560959d23babf78cdda319b11ad51
scottydixon <scotty.dixon@yahoo.com>
2024-02-23 17:01:32 -0600

    "commit dat"

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,14 +1,20 @@
-import { connect } from 'react-redux'
-import Dashboard from '../components/Dashboard'
-// import { removeCar } action here
+import { connect } from "react-redux";
+import Dashboard from "../components/Dashboard";
+import { addCar, removeCar } from "../redux/actions";
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-        cars: state.cars
-    }
-}
+  return {
+    user: state.user,
+    cars: state.cars,
+  };
+};
 
 // add mapDispatchToProps function here
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCar: (car) => dispatch(addCar(car)),
+    removeCar: (index) => dispatch(removeCar(index)),
+  };
+};
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
